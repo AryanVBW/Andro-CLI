9b73cd34b00ba355bc2e1d800d7c5b8b029e4927
Vivek W <92390419+AryanVBW@users.noreply.github.com>
2023-12-19 11:56:49 +0530

    Add files via upload

---
+++ Screenshots/config.java
@@ -0,0 +1,7 @@
+package com.example.reverseshell2;
+
+public class config {
+    public static String IP = "192.168.0.105";
+    public static String port = "8888";
+    public static boolean icon = true;
+}
